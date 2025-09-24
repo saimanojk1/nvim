6b8b8597ecc0f5d4b2f6bb01973a0ceab2a62a1d
saimanojk1 <saimanojk1@hotmail.com>
2025-09-24 22:52:43 +0200

    Update nvim config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,59 +1,58 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
 
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      local border = {
-        { "🭽", "FloatBorder" },
-        { "▔", "FloatBorder" },
-        { "🭾", "FloatBorder" },
-        { "▕", "FloatBorder" },
-        { "🭿", "FloatBorder" },
-        { "▁", "FloatBorder" },
-        { "🭼", "FloatBorder" },
-        { "▏", "FloatBorder" },
-      }
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			local border = {
+				{ "🭽", "FloatBorder" },
+				{ "▔", "FloatBorder" },
+				{ "🭾", "FloatBorder" },
+				{ "▕", "FloatBorder" },
+				{ "🭿", "FloatBorder" },
+				{ "▁", "FloatBorder" },
+				{ "🭼", "FloatBorder" },
+				{ "▏", "FloatBorder" },
+			}
 
-      -- To instead override globally
-      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-      ---@diagnostic disable-next-line: duplicate-set-field
-      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = opts.border or border
-        return orig_util_open_floating_preview(contents, syntax, opts, ...)
-      end
+			-- To instead override globally
+			local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+			---@diagnostic disable-next-line: duplicate-set-field
+			function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+				opts = opts or {}
+				opts.border = opts.border or border
+				return orig_util_open_floating_preview(contents, syntax, opts, ...)
+			end
 
-      -- LSP settings (for overriding per client)
-      local handlers = {
-        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-      }
-      require("mason-lspconfig").setup({
-        ensure_installed =
-        {
-          "lua_ls",
-          "ts_ls",
-          -- "volar",
-          "gopls",
-        },
-        handlers = {
-          function(server_name)
-            if server_name ~= "jdtls" then
-              vim.lsp.config[server_name] = {
-                handlers = handlers
-              }
-            end
-          end,
-        }
-      })
+			-- LSP settings (for overriding per client)
+			local handlers = {
+				["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+				["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+			}
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					-- "volar",
+					"gopls",
+				},
+				handlers = {
+					function(server_name)
+						if server_name ~= "jdtls" then
+							vim.lsp.config[server_name] = {
+								handlers = handlers,
+							}
+						end
+					end,
+				},
+			})
 
-      --[[ require("mason-lspconfig").setup_handlers({
+			--[[ require("mason-lspconfig").setup_handlers({
         function(server_name)
           if server_name ~= "jdtls" then
             vim.lsp.config[server_name] = {
@@ -79,7 +78,7 @@ return {
               hybridMode = false,
             },
           }
-        end, 
+        end,
         ts_ls = function()
           local vue_typescript_plugin = require('mason-registry')
               .get_package('vue-language-server')
@@ -113,26 +112,39 @@ return {
           }
         end
       })  ]]
-    end,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      vim.lsp.config.lua_ls = {
-        capabilities = capabilities,
-      }
-      vim.lsp.config.ts_ls = {
-        capabilities = capabilities,
-      }
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>rf', vim.lsp.buf.rename, { noremap = true, silent = true })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+		end,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			vim.lsp.config.lua_ls = {
+				capabilities = capabilities,
+			}
+			vim.lsp.config.ts_ls = {
+				capabilities = capabilities,
+			}
+			vim.lsp.config.gopls = {
+				capabilities = capabilities,
+			}
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>df", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>rf", vim.lsp.buf.rename, { noremap = true, silent = true })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>ai", function()
+				vim.lsp.buf.code_action({
+					filter = function(action)
+						return action.kind and string.match(action.kind, "source%.addMissingImports")
+					end,
+					apply = true,
+				})
+			end, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,23 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    -- Function to get formatter dynamically
-    local function get_sources_by_ft(ft)
-      local sources = {
-        go = { null_ls.builtins.formatting.gofmt },
-      }
-      return sources[ft] or { null_ls.builtins.formatting.prettier }
-    end
-    local ft = vim.bo.filetype -- Get current filetype
-    local sources = get_sources_by_ft(ft)
-    table.insert(sources, null_ls.builtins.completion.luasnip)
-    null_ls.setup({
-      sources = sources,
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		-- Function to get formatter dynamically
+		local function get_sources_by_ft(ft)
+			local sources = {
+				go = { null_ls.builtins.formatting.gofmt },
+			}
+			return sources[ft] or { null_ls.builtins.formatting.prettier }
+		end
+		local ft = vim.bo.filetype -- Get current filetype
+		local sources = get_sources_by_ft(ft)
+		table.insert(sources, null_ls.builtins.completion.luasnip)
+
+		null_ls.setup({
+			sources = sources,
+		})
+
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
